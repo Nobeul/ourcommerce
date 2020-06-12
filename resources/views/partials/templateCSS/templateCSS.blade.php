02d3fba0257efda86a187d3723002beb3f06cd28
Nobeul <nobeul.cse@gmail.com>
2020-06-12 16:59:00 +0600

    added fashiop template in the project and did mastering to the index file

---
+++ resources/views/partials/templateCSS/templateCSS.blade.php
@@ -0,0 +1,17 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" href="img/favicon.png" type="image/png">
+<title>Fashiop</title>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="fashiop/css/bootstrap.css">
+<link rel="stylesheet" href="fashiop/vendors/linericon/style.css">
+<link rel="stylesheet" href="fashiop/css/font-awesome.min.css">
+<link rel="stylesheet" href="fashiop/vendors/owl-carousel/owl.carousel.min.css">
+<link rel="stylesheet" href="fashiop/vendors/lightbox/simpleLightbox.css">
+<link rel="stylesheet" href="fashiop/vendors/nice-select/css/nice-select.css">
+<link rel="stylesheet" href="fashiop/vendors/animate-css/animate.css">
+<link rel="stylesheet" href="fashiop/vendors/jquery-ui/jquery-ui.css">
+<!-- main css -->
+<link rel="stylesheet" href="fashiop/css/style.css">
+<link rel="stylesheet" href="fashiop/css/responsive.css">
---
+++ resources/views/partials/templateJS/templateJs.blade.php
@@ -0,0 +1,17 @@
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="fashiop/js/jquery-3.2.1.min.js"></script>
+<script src="fashiop/js/popper.js"></script>
+<script src="fashiop/js/bootstrap.min.js"></script>
+<script src="fashiop/js/stellar.js"></script>
+<script src="fashiop/vendors/lightbox/simpleLightbox.min.js"></script>
+<script src="fashiop/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+<script src="fashiop/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+<script src="fashiop/vendors/isotope/isotope-min.js"></script>
+<script src="fashiop/vendors/owl-carousel/owl.carousel.min.js"></script>
+<script src="fashiop/js/jquery.ajaxchimp.min.js"></script>
+<script src="fashiop/vendors/counter-up/jquery.waypoints.min.js"></script>
+<script src="fashiop/vendors/flipclock/timer.js"></script>
+<script src="fashiop/vendors/counter-up/jquery.counterup.js"></script>
+<script src="fashiop/js/mail-script.js"></script>
+<script src="fashiop/js/theme.js"></script>
